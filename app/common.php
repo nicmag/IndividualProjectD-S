c36d2bf4451ed1b7042fdd1f96d41dc6b461285a
Nick Magers <nmagers@iu.edu>
2018-10-09 12:42:31 -0400

    common and comment

--- app/common.php
+++ app/common.php
@@ -3,5 +3,10 @@
 chdir(__DIR__);
 set_include_path (__DIR__);
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST'
+&& stripos($_SERVER['CONTENT_TYPE'], 'application/json') !== false ) {
+  $_POST = json_decode(file_get_contents('php://input'), true);
+}
+
 require 'environment.php';
-require 'models/Homework.php';
+require 'models/Comment.php';
--- app/models/Comment.php
+++ app/models/Comment.php
@@ -3,10 +3,22 @@ class Comment
 {
   public $id;
   public $comment;
-  public function __construct($data) {
-    $this->id = intval($data['id']);
-    $this->comment = $data['comment'];
+  public function __construct($row) {
+    $this->id = isset($row['id']) ? intval($row['id']) : null;
+    // $this->id = intval($data['id']);
+    $this->comment = $row['comment'];
   }
+
+  public function create() {
+  $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+  $sql = 'INSERT Chair (comment)
+          VALUES (?)';
+  $statement = $db->prepare($sql);
+  $success = $statement->execute([
+    $this->comment
+  ]);
+  $this->id = $db->lastInsertId();
+}
 
   public static function fetchAll() {
     // 1. Connect to the database
