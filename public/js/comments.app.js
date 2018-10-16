a93b7d796b796be2288f36b281c68ddc8b3a87ce
Nick Magers <nmagers@iu.edu>
2018-10-16 14:15:16 -0400

    added get

---
+++ public/js/comments.app.js
@@ -0,0 +1,59 @@
+var commentsApp = new Vue({
+  el:'#commentMain',
+  data: {
+
+      comment:[],
+      newCommentForm: {}
+},
+
+methods: {
+  // handleCommentForm(e){
+  //
+  //   const s = JSON.stringify(this.workForm);
+  //     console.log(s);
+  //
+  //   fetch('api/comment.php', {
+  //       method: "POST", // *GET, POST, PUT, DELETE, etc.
+  //       headers: {
+  //           "Content-Type": "application/json; charset=utf-8"
+  //       },
+  //       body: s // body data type must match "Content-Type" header
+  //     })
+  //     .then( response => response.json() )
+  //     .then( json => {this.comment.push(json)})
+  //     .catch( err => {
+  //       console.error('WORK POST ERROR:');
+  //       console.error(err);
+  //     })
+  //     this.workForm = this.getEmptyWorkForm();
+  // },
+ //handle
+//   getEmptyWorkForm() {
+//   return {comment =
+//     team_id: null,
+//     task_id: this.task.id,
+//     completion_estimate: 0
+//   } // return
+// }, //getEmptyWorkForm
+
+getAllComments: function() {
+  fetch('api/comment.php')
+  .then(response => response.json())
+  .then(json => {commentsApp.comment = json;} )
+  .catch( err=>{
+    console.error('COMMENTS FETCH ERROR:');
+    console.error(err);
+  })
+}
+
+},
+  created () {
+    this.getAllComments();
+    // const url = new URL(window.location.href);
+    // const taskId = url.searchParams.get('taskId');
+    // console.log('Task: '+ taskId);
+    // this.task.id = taskId;
+  }
+
+
+});
